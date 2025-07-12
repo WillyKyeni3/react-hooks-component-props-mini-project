fdfce162d6b0e45368a1f144342c81bf78bd2576
willymusyoka <willykyeni@gmail.com>
2025-07-12 14:00:58 +0300

    Enhance Article component to display reading time with emojis based on minutes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,27 @@
 import React from "react";
 
-function Article( { title, date= "January 1, 1970", preview }) {
-    return (
-        <article>
-            <h3>{title}</h3>
-            <small>{date}</small>
-            <p>{preview}</p>
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  // Calculate emoji string based on minutes
+  let emoji = "";
+  let emojiCount = 0;
 
-        </article>
-    );
+  if (minutes < 30) {
+    emojiCount = Math.ceil(minutes / 5);
+    emoji = "☕️".repeat(emojiCount);
+  } else {
+    emojiCount = Math.ceil(minutes / 10);
+    emoji = "🍱".repeat(emojiCount);
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} {emoji} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
 }
 
 export default Article;
